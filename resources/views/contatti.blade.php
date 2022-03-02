29cfa0b92a4f10c7d4fd9088aae85a563591c542
ThubeesanKugathas <Thubeesan@outlook.com>
2022-03-02 15:29:52 +0100

    link to go back to homepage in all components

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/utility.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>CONTATTI</title>
 </head>
 <body>
-    <h1>CONTATTI</h1>
+    <nav class="text-center">
+        <h1 class="my-4">CONTATTI</h1>
+        <a href="/#" id="link">TORNA INDIETRO</a>
+    </nav>
 </body>
 </html>
